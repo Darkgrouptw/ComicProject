eb76b31ec0de3f2a54fc27bb4c6030b41f9f5630
Dark <darkgrouptw@gmail.com>
2016-06-21 16:23:51 +0800

    1. 產生 DLL的 Library 的專案

---
+++ 1. OtsuGaussion_CommandLine/OtsuGaussion_Library/otsugaussion_library.cpp
@@ -0,0 +1,11 @@
+#include "otsugaussion_library.h"
+
+OtsuGaussion_Library::OtsuGaussion_Library()
+{
+
+}
+
+OtsuGaussion_Library::~OtsuGaussion_Library()
+{
+
+}
---
+++ 1. OtsuGaussion_CommandLine/OtsuGaussion_Library/otsugaussion_library.h
@@ -0,0 +1,22 @@
+#ifndef OTSUGAUSSION_LIBRARY_H
+#define OTSUGAUSSION_LIBRARY_H
+
+#include "otsugaussion_library_global.h"
+
+// OpenCV
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+class OTSUGAUSSION_LIBRARY_EXPORT OtsuGaussion_Library
+{
+public:
+	OtsuGaussion_Library();
+	~OtsuGaussion_Library();
+
+private:
+
+};
+
+#endif // OTSUGAUSSION_LIBRARY_H
